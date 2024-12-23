ce708c097d949e89c60ea1a664ddbdb201e7990f
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2024-12-22 17:55:27 -0800

    Add colors

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -1,5 +1,5 @@
 <div class="flex flex-col justify-center">
   <p class="text-base md:text-lg font-medium text-gray-700">
-    Hi! I am Kevin Kodama, a software engineer and writer seeking new ways to explain big ideas. My latest project is a sci-fi & fantasy magazine called <a class="text-blue-600" href="linktr.ee/synthesized.sunsets">Synthesized Sunsets</a>, which explores speculative topics through a romantic lens. Feel free to reach out and send me an email at kevin.kodama (at) protonmail.com!
+    Hi! I am Kevin Kodama, a software engineer and writer seeking new ways to explain big ideas. My latest project is a sci-fi & fantasy magazine called <a class="text-blue-600" href="https://linktr.ee/synthesized.sunsets">Synthesized Sunsets</a>, which explores speculative topics through a romantic lens. Feel free to reach out and send me an email at kevin.kodama (at) protonmail.com!
   </p>
 </div>
--- src/components/Essays.svelte
+++ src/components/Essays.svelte
@@ -9,59 +9,98 @@
     description="In the world of literature, genres constantly rise, fall, divide, and recombine. But two broad categories of books always maintain their relevance: genre fiction and literary fiction. These broad categories are determined not by aesthetics, but by goals. Genre fiction is by far the more popular of the two, and its primary goal is to entertain. While the “genre fiction” label technically applies to any book that fits into one of those shelves at the bookstore (e.g. “Mystery”, “Fantasy”), in practice the label implies that a book is escapist. Its goal is to turn the reader’s attention away from the world by giving their mind somewhere else to live for a while."
     link="https://synthesizedsunsets.substack.com/p/will-science-fiction-ever-be-literary"
     image="/img/literary.jpg"
+    tools={[
+      { name: 'science-fiction', link: '', color: 'bg-green-100 text-green-500 hover:bg-green-500 hover:text-white' },
+      { name: 'literary-fiction', link: '', color: 'bg-purple-100 text-orange-500 hover:bg-purple-500 hover:text-white' },
+      { name: 'genre', link: '', color: 'bg-purple-100 text-yellow-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay
     title="In Caroline M. Yoachim's Fragile Worlds, Acts of Care Take Center Stage"
     description="Caroline M. Yoachim (yo-kum) is one of my favorite Seattle speculative fiction writers. Because of the limits of the form, most of my favorite short stories are equal parts narrative and thought experiment: evocative vehicles for an interesting idea instead of full-on narratives. But Yoachim is the rare author who can consistently pack epic tales into a small space, making 20 pages feel like an entire novel."
     link="https://synthesizedsunsets.substack.com/p/in-caroline-m-yoachims-fragile-worlds"
     image="/img/yoachim.webp"
+    tools={[
+      { name: 'caroline-m-yoachim', link: '', color: 'bg-green-100 text-purple-800 hover:bg-green-500 hover:text-white' },
+      { name: 'fantasy', link: '', color: 'bg-purple-100 text-purple-500 hover:bg-purple-500 hover:text-white' },
+      { name: 'motherhood', link: '', color: 'bg-purple-100 text-red-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay
     title="'Grand Nostalgia' and the Temporal Injustice of 'Castle in the Sky'"
-    description="When I first watched the Studio Ghibli film Castle in the Sky, I really didn’t like it. It took me years to find the words for why. Castle in the Sky is an animated film about the ancient floating city of Laputa. Laputa is said to be a utopia with advanced technology and valuable treasure, making it a prize for explorers of all stripes. The film’s protagonists have a jewel that can locate the city, so they are constantly evading ambitious raiders who hope to gain the power of Laputa for themselves. But when the main characters finally arrive at Laputa, it turns out to be an abandoned wreck. Powerful yet dormant technology is the only lasting legacy of their once-mighty civilization. As the raiders vie for control of the city, they end up unleashing powerful forces they don’t understand. The subsequent struggle sends Laputa into a spiral of self-destruction."
+    description="When I first watched the Studio Ghibli film Castle in the Sky, I really didn’t like it. It took me years to find the words for why. Castle in the Sky is an animated film about the ancient floating city of Laputa. Laputa is said to be a utopia with advanced technology and valuable treasure, making it a prize for explorers of all stripes. The film’s protagonists have a jewel that can locate the city, so they are constantly evading ambitious raiders who hope to gain the power of Laputa for themselves. But when the main characters finally arrive at Laputa, it turns out to be an abandoned wreck. Powerful yet dormant technology is the only lasting legacy of their once-mighty civilization."
     link="https://synthesizedsunsets.substack.com/p/grand-nostalgia-and-the-temporal"
     image="/img/castle.webp"
+    tools={[
+      { name: 'nostalgia', link: '', color: 'bg-green-100 text-purple-100 hover:bg-green-500 hover:text-white' },
+      { name: 'civilization', link: '', color: 'bg-purple-100 text-purple-600 hover:bg-purple-500 hover:text-white' },
+      { name: 'miyazaki', link: '', color: 'bg-purple-100 text-purple-700 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay
     title="Beaches of Rebirth and Memory"
     description="When I think about beaches, I instinctively think of a beach day: unstable umbrellas, sprawled-out towels, and kids shouting amid the waves. Beach days have a relaxing commotion to them— there’s enough peace for them to be soothing, but also enough activity to drown out bothersome thoughts. But beaches take on an entirely new character in the absence of crowds. When I’m on the beach alone, the vast planes of sand and water have a way of encouraging deep thought. Eventually I start to feel this pull, not unlike the “call of the void” that comes when standing on the edge of a cliff. It’s like the depths are calling out to me, singing a siren song."
     link="https://synthesizedsunsets.substack.com/p/beaches-of-rebirth-and-memory"
     image="/img/beaches.jpg"
+    tools={[
+      { name: 'memory', link: '', color: 'bg-green-100 text-purple-300 hover:bg-green-500 hover:text-white' },
+      { name: 'dreams', link: '', color: 'bg-purple-100 text-purple-400 hover:bg-purple-500 hover:text-white' },
+      { name: 'pokemon', link: '', color: 'bg-purple-100 text-purple-200 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay
     title="Ships Create the Rhythm of Adventure"
     description="Adventure is a very general term, but I think of it as a narrative that comes from intentionally striving towards wonder. An epic journey may start out of necessity, but it does not become an adventure until someone embraces their inner adventurous desire. Good adventures need both stress and stability. The ideal adventure protagonist has a stable life: not stable in any specific sense, but just disappointingly consistent in some way. This is where that adventurous desire comes from! Stress is needed to light the fire of adventure, but without stability there is no fuel."
     link="https://synthesizedsunsets.substack.com/p/ships-create-the-rhythm-of-adventure"
     image="/img/adventure.webp"
+    tools={[
+      { name: 'adventure', link: '', color: 'bg-green-100 text-green-300 hover:bg-green-500 hover:text-white' },
+      { name: 'star-wars', link: '', color: 'bg-purple-100 text-green-100 hover:bg-purple-500 hover:text-white' },
+      { name: 'voyages', link: '', color: 'bg-purple-100 text-green-200 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay 
     title="Dissonance and Danger in 'The Princess and the Clock'"
     link="https://synthesizedsunsets.substack.com/p/dissonance-and-danger-in-the-princess"
-    description="One of the best songs to come out of the pandemic years was 'The Princess and the Clock' by Kero Kero Bonito. It has the sound of a video game, the themes of early quarantine, and the lyrics of an ancient myth. This union of future, present, and past became the common thread for KKB’s compilation album Civilization. According to Kero Kero Bonito, the track 'is the tale of a young explorer who is kidnapped while sailing the world, imprisoned at the top of a tower and worshipped as royalty by an isolated society. Trapped in her chamber, she spends years dreaming of escaping, until one day she disappears.'. Despite its heavy subject matter, “The Princess and the Clock” is a lush and hopeful track that is filled with the spirit of adventure."
+    description="One of the best songs to come out of the pandemic years was 'The Princess and the Clock' by Kero Kero Bonito. It has the sound of a video game, the themes of early quarantine, and the lyrics of an ancient myth. This union of future, present, and past became the common thread for KKB’s compilation album Civilization. According to Kero Kero Bonito, the track 'is the tale of a young explorer who is kidnapped while sailing the world, imprisoned at the top of a tower and worshipped as royalty by an isolated society. Trapped in her chamber, she spends years dreaming of escaping, until one day she disappears.'."
     image="/img/princess.webp"
+    tools={[
+      { name: 'pop-music', link: '', color: 'bg-green-100 text-green-500 hover:bg-green-500 hover:text-white' },
+      { name: 'adventure', link: '', color: 'bg-purple-100 text-purple-300 hover:bg-purple-500 hover:text-white' },
+      { name: 'captivity', link: '', color: 'bg-purple-100 text-purple-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay
     title="'The Wandering Inn' Allows Us to Properly Fear the Sea"
-    description="The Wandering Inn is one of the greatest stories ever told. It is a fantasy web novel that updates weekly— think The Pickwick Papers for the 21st century. Since its inception in 2016, The Wandering Inn has amassed nearly 8,000 Patreon subscribers and over 13 million words. The Wandering Inn is part of the isekai genre, which focuses on 'a displaced person or people who are transported to and have to survive in another world, such as a fantasy world, game world, or parallel universe without the possibility of returning to their original world'. Isekai is popular in Japanese media, but many works in the genre have been criticized for devolving into shallow male power fantasies."
+    description="The Wandering Inn is one of the greatest stories ever told. It is a fantasy web novel that updates weekly— think The Pickwick Papers for the 21st century. Since its inception in 2016, The Wandering Inn has amassed nearly 8,000 Patreon subscribers and over 13 million words. The Wandering Inn is part of the isekai genre, which focuses on 'a displaced person or people who are transported to and have to survive in another world, such as a fantasy world, game world, or parallel universe without the possibility of returning to their original world'."
     link="https://synthesizedsunsets.substack.com/p/the-wandering-inn-allows-us-to-properly"
     image="/img/inn.webp"
+    tools={[
+      { name: 'web-fiction', link: '', color: 'bg-green-100 text-yellow-300 hover:bg-green-500 hover:text-white' },
+      { name: 'wandering-inn', link: '', color: 'bg-purple-100 text-yellow-100 hover:bg-purple-500 hover:text-white' },
+      { name: 'genre', link: '', color: 'bg-purple-100 text-yellow-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay 
     title="Was 'Radical Optimism' a Flop?"
     link="https://synthpop.substack.com/p/was-radical-optimism-a-flop"
     description="Some artists have diehard fanbases that will always come out for them. This is true of certain Main Pop Girls like Taylor Swift, but it is also true of less popular artists who are thus able to punch above their weight. Even though Ed Sheeran is a fair bit more popular than Nicki Minaj, he still has more flop potential due to his more casual fanbase. You could say Sheeran’s fanbase is more fragile. Like Sheeran, Perry had a more fragile fanbase. And so after Witness, her house of cards came tumbling down. I’m not saying anything crazy here; artists with more dedicated fanbases are obviously going to have more success. But I’d like to raise a deeper question: why do some artists have a more dedicated fanbase? And what can artists do to cultivate that image?"
     image="/img/snowball.webp"
+    tools={[
+      { name: 'pop-music', link: '', color: 'bg-green-100 text-red-500 hover:bg-green-500 hover:text-white' },
+      { name: 'fandom', link: '', color: 'bg-purple-100 text-blue-300 hover:bg-purple-500 hover:text-white' },
+      { name: 'sociology', link: '', color: 'bg-purple-100 text-blue-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
   <Essay 
     title="On Spotify, *YOU* Are the Genre"
     link="https://synthpop.substack.com/p/on-spotify-you-are-the-genre"
     description="If you’ve paid close attention to Spotify genres, you might have noticed a genre called 'escape room'. There are other unfamiliar genres, like 'metropopolis', and 'chamber pop', but those at least call to mind a specific vibe. “Escape room' sounds completely nonsensical. This has spawned a number of think pieces about what this term means. These articles make valid points, but a true understanding of escape room requires us to throw out the conventional understanding of genre. Ultimately, the reason for this confusion is a very recent shift in the way genre is defined."
     image="/img/genre.webp"
-  />
-  <Essay 
-    title="The Rise, Fall, and Return of Future Nostalgia"
-    link="https://synthpop.substack.com/p/great-eras-the-rise-fall-and-rebirth"
-    description="It’s been three years since the release of Future Nostalgia, the album that put Dua Lipa on top of the pop world. Future Nostalgia was the soundtrack of quarantine for many, and it was widely considered one of the best albums of the new decade. Despite Future Nostalgia’s staying power, it didn’t do opening numbers like you might expect. In fact, the album netted zero number ones. And the album itself didn’t do great numbers either — it peaked at #3 in the US and didn’t even debut at #1 in the UK. These numbers didn’t matter in the end, as Future Nostalgia is now the sixth-most streamed album of all time, and Dua Lipa has enough name recognition to headline the soundtrack of the most anticipated movie in the world. But it demonstrates that the success of Future Nostalgia was far from a foregone conclusion."
-    image="/img/nostalgia.webp"
+    tools={[
+      { name: 'pop-music', link: '', color: 'bg-green-100 text-red-500 hover:bg-green-500 hover:text-white' },
+      { name: 'sociology', link: '', color: 'bg-purple-100 text-blue-500 hover:bg-purple-500 hover:text-white' },
+      { name: 'genre', link: '', color: 'bg-purple-100 text-yellow-500 hover:bg-purple-500 hover:text-white' }
+    ]}
   />
 </section>
